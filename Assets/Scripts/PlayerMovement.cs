ab8bad8a103b780faf05c0cd3163361246e0efbf
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2022-09-21 17:03:22 -0300

    Tank Rotation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 1;
+    float rotationSpeed = 25;
+    Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TankMovement();
+    }
+
+    // ------------------------ Tank Chasis Movement ------------------------- \\
+    void TankMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            float rotation = -1f * rotationSpeed;
+            transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            float rotation = 1f * rotationSpeed;
+            transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        }
+    }
+
+    // ----------------------- Tank Turret Movement ------------------------------ \\
+
+    void turretMovement()
+    {
+        // aim done indipendantly
+        // then turret just looks at target
+    }
+
+}
