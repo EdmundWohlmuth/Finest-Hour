32bf1e9984afa58a9e47a938dfe602ca4b080fc9
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2022-10-07 11:34:33 -0300

    Prepping Game Manager

---
+++ Assets/Scripts/GameManager/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("User Interface Screens")]
+    public Canvas _MainMenu;
+    public Canvas _Upgrade;
+    public Canvas _GamePlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
